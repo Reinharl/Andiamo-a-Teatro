2eebdefbda59742fcf0bf1a12c38bc8a2de5f190
Reinharl <f.depinto00@gmail.com>
2023-07-03 10:20:42 +0200

    Init project

---
+++ src/test/java/com/spring/andiamo_a_teatro/AndiamoATeatroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.andiamo_a_teatro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndiamoATeatroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
